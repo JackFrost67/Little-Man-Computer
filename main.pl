2c670bd9651c24d13817167c58540f3592e90da1
JackFrost67 <32344285+JackFrost67@users.noreply.github.com>
2018-12-21 22:19:46 +0100

    Delete main.pl

--- main.pl
+++
@@ -1,6 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%            INIZIO DEL PROGRAMMA                   %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-:- [lmc_load].          % LMC Load
-:- [lmc_parser].        % LMC Parser
-:- [lmc_assembler].     % LMC Assembler
